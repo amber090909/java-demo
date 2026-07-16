ed8ad55eff0d305e87068ab0adef908235788272
amber0909 <17326787+amber0909@gitee.com>
2026-07-16 15:16:52 +0800

    add color feature

--- src/Circle.java
+++ src/Circle.java
@@ -1,8 +1,18 @@
 public class Circle implements Shape {
 	private double r ;
+	private String color;
+
+	public Circle (double r,String color) {
+		this.r = r ;
+		this.color=color ;
+	}
 
 	public Circle (double r) {
 		this.r = r ;
+	}
+	
+	public void showColor(){
+		System.out.println("颜色"+color);
 	}
 
 	@Override
